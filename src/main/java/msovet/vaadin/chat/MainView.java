234fc590a98b38c16b4b3c3664e2860362dc7803
msovet <merei9822@gmail.com>
2021-11-25 15:42:58 +0600

    Initial commit

---
+++ src/main/java/msovet/vaadin/chat/MainView.java
@@ -0,0 +1,9 @@
+package msovet.vaadin.chat;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+    
+}
